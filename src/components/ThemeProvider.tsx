9512bba620a6636319b0a3cc27f2864dcba8d8b1
Phantom2006-dot <danielnworah2006@gmail.com>
2025-12-02 14:05:39 -1100

    Update ThemeProvider.tsx

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -5,10 +5,9 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider 
       attribute="class"
-      defaultTheme="dark"
-      enableSystem={false} // Disable system theme
-      forcedTheme="dark" // This forces dark mode ALWAYS
-      disableTransitionOnChange
+      defaultTheme="dark"  // Default to dark on first visit
+      enableSystem={true}  // Allow system preference detection
+      disableTransitionOnChange={false}
       {...props}
     >
       {children}
